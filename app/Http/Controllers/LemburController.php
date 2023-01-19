36cc3ebe3804cd8ef7506acc12b235b41742f74e
Kizmine <berli_122@smkassalaambandung.sch.id>
2023-01-19 11:41:29 +0700

    update #6

--- app/Http/Controllers/LemburController.php
+++ app/Http/Controllers/LemburController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Lembur;
 use Illuminate\Http\Request;
-use Alert;
+use RealRashid\SweetAlert\Facades\Alert;
 use Illuminate\Support\Facades\Auth;
 
 class LemburController extends Controller
