66e9ae7bd1985b512a76b68961bfa078dec1470a
Kizmine <97926227+berli122@users.noreply.github.com>
2023-02-19 16:39:08 +0700

    Update 9.1

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,10 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
+Route::get('/register', function () {
+    return back( );
+});
+
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 // Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
